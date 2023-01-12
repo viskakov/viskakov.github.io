f32899ba877d36a76c981dc40c32a3c793cec436
Vsevolod Iskakov <vsev0l0d.vsevolod@gmail.com>
2023-01-12 21:40:54 +0300

    Improved switch theme button

--- js/main.ts
+++ js/main.ts
@@ -1,21 +1,20 @@
-var themeButton = document.querySelector("#button");
+const themeButton = document.querySelector("#button");
 themeButton.addEventListener("click", changeTheme);
 themeButton.addEventListener("click", changeText);
+
 function changeTheme() {
     if (document.documentElement.hasAttribute("theme")) {
         document.documentElement.removeAttribute("theme");
-    }
-    else {
+    } else {
         document.documentElement.setAttribute("theme", "dark");
     }
 }
+
 function changeText() {
-    var button = document.getElementById("button");
+    const button = document.getElementById("button");
     if (document.documentElement.hasAttribute("theme")) {
         button.innerText = "Светлая тема";
-    }
-    else {
+    } else {
         button.innerText = "Тёмная тема";
     }
 }
-//# sourceMappingURL=main.js.map
