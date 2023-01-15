42fb7b6e5417d962383f1c31058c53f924a5dc6b
Vsevolod Iskakov <vsev0l0d.vsevolod@gmail.com>
2023-01-15 22:38:29 +0300

    Added localStorage for DarkTheme

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 var themeButton = document.querySelector("#button");
-themeButton.addEventListener("click", changeTheme);
-themeButton.addEventListener("click", changeText);
-function changeTheme() {
+function toggleDarkTheme(state) {
     var body = document.body;
     body.classList.toggle("darkTheme");
+    localStorage.setItem("isDarkTheme", state);
+    changeButtonText();
 }
-function changeText() {
+function changeButtonText() {
     var button = document.getElementById("button");
     if (document.body.classList.contains("darkTheme")) {
         button.innerHTML = "<i id=\"themeIcon\" class=\"bi bi-sun-fill\"></i>Светлая тема";
@@ -14,4 +14,17 @@ function changeText() {
         button.innerHTML = "<i id=\"themeIcon\" class=\"bi bi-moon-fill\"></i>Тёмная тема";
     }
 }
+var isDarkTheme = localStorage.getItem("isDarkTheme");
+if (isDarkTheme === "1") {
+    toggleDarkTheme("1");
+}
+themeButton.addEventListener("click", function () {
+    var isDarkTheme = localStorage.getItem("isDarkTheme");
+    if (isDarkTheme === "0") {
+        toggleDarkTheme("1");
+    }
+    else {
+        toggleDarkTheme("0");
+    }
+});
 //# sourceMappingURL=main.js.map
--- js/main.ts
+++ js/main.ts
@@ -1,13 +1,13 @@
 const themeButton = document.querySelector("#button");
-themeButton.addEventListener("click", changeTheme);
-themeButton.addEventListener("click", changeText);
 
-function changeTheme() {
+function toggleDarkTheme(state: string) {
     const body = document.body;
     body.classList.toggle("darkTheme");
+    localStorage.setItem("isDarkTheme", state)
+    changeButtonText()
 }
 
-function changeText() {
+function changeButtonText() {
     const button = document.getElementById("button");
     if (document.body.classList.contains("darkTheme")) {
         button.innerHTML = "<i id=\"themeIcon\" class=\"bi bi-sun-fill\"></i>Светлая тема";
@@ -15,3 +15,17 @@ function changeText() {
         button.innerHTML = "<i id=\"themeIcon\" class=\"bi bi-moon-fill\"></i>Тёмная тема";
     }
 }
+
+let isDarkTheme = localStorage.getItem("isDarkTheme");
+if (isDarkTheme === "1") {
+    toggleDarkTheme("1")
+}
+
+themeButton.addEventListener("click", () => {
+    let isDarkTheme = localStorage.getItem("isDarkTheme");
+    if (isDarkTheme === "0") {
+        toggleDarkTheme("1");
+    } else {
+        toggleDarkTheme("0");
+    }
+})
